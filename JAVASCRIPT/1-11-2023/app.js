2f8b4d0a05f22cf63e9ef236b3b12d1668df3e7c
furqan <furqan.azeem369@gmail.com>
2023-11-16 11:35:24 +0500

    code added

--- JAVASCRIPT/1-11-2023/app.js
+++ JAVASCRIPT/1-11-2023/app.js
@@ -1,10 +1,28 @@
-function submit(){
-    var text = document.getElementById("text");
-    console.log(text.value)
+// function submit(){
+//     var text = document.getElementById("text");
+//     console.log(text.value)
+// }
+
+// var email = prompt("enter your email");
+// var password = prompt("enter your password");
+
+
+
+
+var input = prompt("name")
+
+if(input == "furqan@gmail.com"){
+    var password = prompt("password")
+        if(password == "245") {
+            console.log("login suceesfully")
+        
+    }
+    else{
+        console.log("enter correct Password")
+    }
+
 }
 
-var email = prompt("enter your email");
-var password = prompt("enter your password");
-
-
-
+else{
+    console.log("enter correct email")
+}
